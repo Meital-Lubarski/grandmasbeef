8964f8471d6291801224facaf44af7b5063cd5ba
May-Shvarts <may1036755@gmail.com>
2026-04-14 15:58:53 +0300

    deleting expensive method

--- Assets/Scripts/UI/Text/ScoreUI.cs
+++ Assets/Scripts/UI/Text/ScoreUI.cs
@@ -8,13 +8,11 @@ public class ScoreUI : MonoBehaviour
 
     private string _player1Name;
     private string _player2Name;
-    private ScoreManager _scoreManager;
     
     private void Start()
     {
         _player1Name = PlayerPrefs.GetString("Player1Name", "Player 1");
         _player2Name = PlayerPrefs.GetString("Player2Name", "Player 2");
-        _scoreManager = FindObjectOfType<ScoreManager>();
         
         UpdateScoreUI(0, 0);
     }
@@ -31,8 +29,6 @@ public class ScoreUI : MonoBehaviour
 
     private void UpdateScoreUI(int player1Hits, int player2Hits)
     {
-        if (_scoreManager == null) return;
-
         // מציג: שם שחקן | ניצחונות במפות: X | פגיעות בסיבוב: Y/6
         player1ScoreText.text = $"{_player1Name}\nMaps Won: {ScoreManager.Instance.Player1MapWins}/2\nHits: {player1Hits}/6";
         player2ScoreText.text = $"{_player2Name}\nMaps Won: {ScoreManager.Instance.Player2MapWins}/2\nHits: {player2Hits}/6";
